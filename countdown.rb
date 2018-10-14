1b66812cf3f7e39570e3b403ff4e9044e3460c2b
Jacob Feldman <jacobgfeld@gmail.com>
2018-10-14 02:27:10 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -1,10 +1,6 @@
 #write your code here
 
 def countdown(count)
-  if(count == 12)
-    puts "HAPPY NEW YEAR!"
-    return 0
-  end
   while (count > 0) do
     puts "#{count} SECOND(S)!"
     count -= 1
